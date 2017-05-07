cceda605307ce1069a024bdcd49689df6fa4b302
MrDSGC <dcheng47@gmail.com>
2017-05-07 12:41:31 -0700

    twitch watch

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import MainPage from './main_page/main_page'
+
+
 const Root = ({ store }) => {
 
   return(
