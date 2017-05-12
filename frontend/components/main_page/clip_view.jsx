e402bc21cfd98149a16d9e01718a5a950fd404e8
MrDSGC <dcheng47@gmail.com>
2017-05-11 19:39:24 -0700

    statefix

---
+++ frontend/components/main_page/clip_view.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ClipView extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render () {
+    return(
+      <div>
+      </div>
+    )
+  };
+}
+
+export default ClipView;
