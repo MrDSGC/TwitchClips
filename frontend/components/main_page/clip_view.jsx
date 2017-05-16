294f5c6d7775476bfe40ee7c64e0fda1ef1eb148
MrDSGC <dcheng47@gmail.com>
2017-05-16 14:12:11 -0700

    clip add

--- frontend/components/main_page/clip_view.jsx
+++ frontend/components/main_page/clip_view.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 class ClipView extends React.Component {
   constructor(props) {
     super(props)
-
   }
 
   render () {
     return(
       <div>
+        {this.props.clip}
       </div>
     )
   };
