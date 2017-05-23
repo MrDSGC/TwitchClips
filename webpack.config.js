d8dc9c3bf9a9935232a31a9703bf0886f7327627
MrDSGC <dcheng47@gmail.com>
2017-05-23 15:43:50 -0700

    webpack prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: "./frontend/twitch_watch.jsx",
   output: {
-    filename: "./bundle.js"
+    filename: "bundle.js"
   },
   module: {
     loaders: [
