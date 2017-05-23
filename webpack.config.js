7d72b9ab1ed7705e87a0cb38d50ee9f0f574194d
MrDSGC <dcheng47@gmail.com>
2017-05-23 15:04:18 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  context: __dirname,
   entry: "./frontend/twitch_watch.jsx",
   output: {
-    path: path.resolve(__dirname),
     filename: "bundle.js"
   },
   module: {
